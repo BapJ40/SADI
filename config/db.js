51ecfb38815b7dc76de060c0b11b0e91441dc972
Jose Baptista <gjr776@gmail.com>
2025-02-10 23:30:41 -0400

    SADI 1.0

--- config/db.js
+++ config/db.js
@@ -4,10 +4,10 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME,
+  database: process.env.DB_NAME || 'login_system',
 });
 
 module.exports = pool.promise();
